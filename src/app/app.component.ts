638a6429b373c5571c9146d4a0dc0d12767ec389
Emery Barnes <ebarnes@buxtonco.com>
2017-08-24 16:47:10 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app';
+  constructor( public sanitizer: DomSanitizer){}
+
+  userName: string ='ebarnes';
+  buxtonImg: string = 'assets/buxton-logo.svg';
+  selectedView:string = '';
+
+  apps = [{name: 'Platform', image: './assets/platform-icon.svg'},
+          {name: 'Nexus', image: './assets/nexus-icon.svg'},
+          {name: 'Ops Dashboard', image: './assets/ops-icon.svg'},
+          {name: 'Finance', image: './assets/finance-icon.png'},
+          {name: 'Marketing', image: './assets/marketing-icon.svg'},
+          {name: 'World View', image: './assets/worldview-icon.svg'}
+        ];
+
+  setView(appName:string):void{
+    this.selectedView = appName;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import { NexusComponent } from './nexus.component';
+import { CompanyComponent } from './company.component';
+import { PlatformLeftNav } from './platform-left-nav.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NexusComponent,
+    CompanyComponent,
+    PlatformLeftNav
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
